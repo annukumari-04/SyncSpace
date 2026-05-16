6c3ff58d03cf5ef3bf21ed3558e00e8963da5a68
annukumari-04 <annu.kumariint17@ethara.ai>
2026-05-16 17:15:49 +0530

    Updated api service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'http://localhost:5005/api',
 });
 
 // Request Interceptor: Add token to headers
